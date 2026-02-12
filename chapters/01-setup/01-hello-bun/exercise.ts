933b14343f0fe7fab2a45a5dbfdb842868f94269
Manpreet Kunnath <manpreetkunnath@gmail.com>
2026-02-12 21:00:30 +0530

    Ch01:L01

--- chapters/01-setup/01-hello-bun/exercise.ts
+++ chapters/01-setup/01-hello-bun/exercise.ts
@@ -4,15 +4,13 @@
  * Your first Bun program! Implement the functions below.
  */
 
-// TODO: Implement the greet function
 // It should take a name parameter and return:
 // "Hello, {name}! Welcome to the Task Manager API."
 export function greet(name: string): string {
-  throw new Error("Not implemented");
+  return `Hello, ${name}! Welcome to the Task Manager API.`
 }
 
-// TODO: Implement getBunVersion
 // It should return the current Bun version using the Bun.version API
 export function getBunVersion(): string {
-  throw new Error("Not implemented");
+  return Bun.version;
 }
